391826795b3ecb2e73b42b344941735858bfb65a
Shiloh <austyn.you@gmail.com>
2024-07-26 12:31:17 +0900

    add dummy txt

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Yeah Docker Compose</p>
+        <p>Yeah Docker Compose asdfasdf</p>
         <a
           className="App-link"
           href="https://reactjs.org"
